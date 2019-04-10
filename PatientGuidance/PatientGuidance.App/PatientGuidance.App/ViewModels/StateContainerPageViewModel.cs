1759096e54457308aed76047ee6a22fb98741b9c
Michael Gotfrid <m.gotya@gmail.com>
2019-04-10 15:31:32 +0300

    .

--- PatientGuidance/PatientGuidance.App/PatientGuidance.App/ViewModels/StateContainerPageViewModel.cs
+++ PatientGuidance/PatientGuidance.App/PatientGuidance.App/ViewModels/StateContainerPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using PatientGuidance.App.Services;
+﻿using System;
+using PatientGuidance.App.Services;
 using Prism.Navigation;
 
 namespace PatientGuidance.App.ViewModels
@@ -6,11 +7,17 @@ namespace PatientGuidance.App.ViewModels
 	public class StateContainerPageViewModel : ViewModelBase
 	{
         private readonly IInstructionCardsProvider _provider;
+        public Action OnReady { get; set; } = () => { };
 
         public StateContainerPageViewModel(INavigationService navigationService, IInstructionCardsProvider provider) 
             : base(navigationService)
         {
             _provider = provider;
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            OnReady();
         }
     }
 }
--- PatientGuidance/PatientGuidance.App/PatientGuidance.App/Views/StateContainerPage.xaml.cs
+++ PatientGuidance/PatientGuidance.App/PatientGuidance.App/Views/StateContainerPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using PatientGuidance.App.ViewModels;
+using Syncfusion.XForms.TabView;
+using Xamarin.Forms;
 
 namespace PatientGuidance.App.Views
 {
@@ -8,6 +10,38 @@ namespace PatientGuidance.App.Views
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+            if (BindingContext is StateContainerPageViewModel ctx)
+            {
+                ctx.OnReady = Loaded;
+            }
+        }
+
+        private void Loaded()
+        {
+            var tabView = new SfTabView();
+            Grid allContactsGrid = new Grid { BackgroundColor = Color.Red };
+            Grid favoritesGrid = new Grid { BackgroundColor = Color.Green };
+            Grid contactsGrid = new Grid { BackgroundColor = Color.Blue };
+            var tabItems = new TabItemCollection
+            {
+                new SfTabItem()
+                {
+                    Title = "Calls",
+                    Content = allContactsGrid
+                },
+                new SfTabItem()
+                {
+                    Title = "Favorites",
+                    Content = favoritesGrid
+                },
+                new SfTabItem()
+                {
+                    Title = "Contacts",
+                    Content = contactsGrid
+                }
+            };
+            tabView.Items = tabItems;
+            this.Content = tabView;
         }
     }
 }
